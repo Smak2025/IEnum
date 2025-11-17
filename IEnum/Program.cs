aa42e1adabbe03141829e64dffd218ed4fec8efe
Smak2025 <smak-80+2025@yandex.ru>
2025-11-17 11:43:13 +0300

    Добавьте файлы проекта.

---
+++ IEnum/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Diagnostics;
+public enum NodesType{
+    Equidistant,Chebyshev,Random
+}
+public class Nodes : IReadOnlyCollection<double>
+{
+    double _left;
+    double _right;
+    int _count;
+    NodesType _type;
+    public int Count => _count;
+
+    public Nodes(double left,double right, int count,NodesType type)
+    {
+        if(left >= right)
+        {
+            throw new ArgumentException("Левый конец должен быть меньше правого");
+        }
+        if(count <= 1)
+        {
+            throw new ArgumentException("Узлов должно быть больше");
+        }
+        _left = left;
+        _right = right;
+        _count = count;
+        _type = type;
+    } 
+
+    public IEnumerator<double> GetEnumerator()
+    {
+        switch (_type)
+        {
+            case NodesType.Equidistant:
+            {
+                    var distance = (_right - _left) / (_count - 1);
+                    for (int i = 0; i < _count; i++)
+                    {
+                        yield return _left + distance * i;
+                    }
+                    break;
+            }
+            case NodesType.Chebyshev:
+            {
+
+                    break;
+            }
+            case NodesType.Random:
+            {
+
+                    break;   
+            }
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
